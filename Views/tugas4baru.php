2821f52be7984446bfefc0bb294ec0d5fbb247c9
najwaamandadesyari <najwaamandadesyari@gmail.com>
2026-04-22 21:13:18 +0700

    Views/tugas4baru.php index.php index1.php

---
+++ Views/tugas4baru.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<table border="1">
+    <tr>
+        <th colspan="4">Daftar Smartphone</th>
+    </tr>
+
+    <tr>    
+        <th>No</th>
+        <th>Nama Brand</th>
+        <th>Gambar</th>
+        <th>Keterangan</th>
+    </tr>
+
+    <tr>
+        <td align="center"rowspan="4">1.</td>
+        <td align="center" rowspan="4">Apple</td>
+        <td rowspan="4">
+            <img src="https://www.apple.com/newsroom/images/2024/09/apple-debuts-iphone-16-pro-and-iphone-16-pro-max/tile/Apple-iPhone-16-Pro-hero-geo-240909-lp.jpg.news_app_ed.jpg" 
+            width="80" height="80">
+        </td>
+    
+                <tr><td>iPhone 16 Pro Max</td></tr>
+                <tr><td>Dengan desain titanium yang memukau. Kontrol kamera, Dolby Vision 4K 120 fps, dan chip A18 Pro.</td></tr>
+                <tr><td>Rp 20.000.000</td></tr>
+    </tr>
+
+    
+    <tr>
+        <td align="center" rowspan="4">2.</td>
+        <td align="center" rowspan="4">Samsung</td>
+        <td rowspan="4">
+            <img src="https://images.samsung.com/id/smartphones/galaxy-s24-ultra/images/galaxy-s24-ultra-highlights-kv.jpg" 
+            width="80" height="80">
+        </td>
+        
+                <tr><td>Samsung Galaxy S24 Ultra</td></tr>
+                <tr><td>HP premium dengan layar besar, S Pen, kamera canggih, dan performa yang sangat mewah</td></tr>
+                <tr><td>Rp 18.000.000</td></tr>
+    
+    
+    </tr>
+
+    
+    <tr>
+        <td align="center" rowspan="4">3.</td>
+        <td align="center" rowspan="4">Xiaomi</td>
+        <td rowspan="4">
+            <img src="https://files.eci.id/documents/product/webmi-12-8256kpp/1663063934-1.webp" 
+            width="80" height="80">
+        </td>
+       
+                <tr><td>Xiaomi 12 8/256</td></tr>
+                <tr><td>HP dengan performa kencang, desain elegan, dan harga terjangkau</td></tr>
+                <><td>Rp 8.000.000</td></tr>
+
+    </tr>
+
+</table>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Judul Halaman</title>
+
+</head>
+<body>
+    <!--Heading/ Judul-->
+    hello  Word
+    <h1>Belajar HTML H1</h1>
+    <h2>Belajar HTML H2</h2>
+    <h3>Belajar HTML H3</h3>
+    <h4>Belajar HTML H4</h4>
+    <h5>Belajar HTML H5</h5>
+    <h6>Belajar HTML H6</h6>
+    <hr>
+    <!----paragraf-->
+    <p align="center">Ini paragraf pertama</p>
+    <b>bold teks</b><br>
+    <strong>teks tebal</strong><br>
+    <i>teks miring</i><br>
+    <code>teks code</code><br>
+    <!---link-->
+    <a href="http://localhost/PemrogramanWeb/index.php">google</a><br>
+
+        <!---- paragarf--->
+    <ol>
+       <li>list pertama</li>
+       <li>list kedua</li>
+       <li>list ketiga</li>
+         
+    </ol>
+
+    <ul>
+       <li>list pertama</li>
+       <li>list kedua</li>
+       <li>list ketiga</li>
+         
+    </ul>
+
+
+    <!---image-->
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5exkjxUI4rQhYRXDGbnawZNAv31cmX81dzA&s" alt="logo unsiq", width="100", height="100">
+
+<br>
+    <table border="1">
+        <tr>
+            <th>Nama</th>
+            <th>Umur</th>
+            <th>Alamat</th>
+        </tr>
+        <tr>
+            <td>John Doe</td>
+            <td>20</td>
+            <td>Jakarta</td>
+        </tr>
+        <tr>
+            <td>Jane Smith</td>
+            <td>19</td>
+            <td>Bandung</td>
+        </tr>
+    </table>
+    <br>
+    <table border="1" width="100%">
+
+        <tr>
+            <td width="10%">
+                <img src="https://1.bp.blogspot.com/-yAQgSUZf2oY/VRIVSGc8rvI/AAAAAAAABA0/DkDFFf71muY/s1600/Logo+UNSIQ+Polos.jpg" alt="LOGO Adidas", width="100", height="100">
+            </td>
+            <td width="90%">
+                <h2>SELAMAT DATANG DI MATA KULIAH PEMROGRAMAN WEB</h2>
+                <h3>Pertemuan 2: HTML Dasar</h3>
+            </td>
+        </tr>
+    </table>
+    <p align="center">
+        Mata kuliah ini memperkenalkan dasar-dasar pemrograman web menggunakan HTML, CSS, dan JavaScript.
+    </p>
+    <p align="center">
+        Pada pertemuan ini, kita fokus pada struktur dan elemen dasar HTML.
+    </p>
+    <hr>
+    <p>
+        HTML adalah bahasa markup yanhg digunakan untuk membuat halaman web. Dengan HTML, kita bisa menampilkan teks, gambar, dan link secara terorganisir. Untuk informasi lengkap dari HTML dapat berkunjung di link berikut <a href="https://id.wikipedia.org/wiki/HTML">Penjelasan HTML</a>
+    </p>
+    <h2>Topik Utama Pertemuan Ini:</h2>
+    <ul>
+        <li>Struktur HTML</li>
+        <li>Heading dan Paragraf</li>
+        <li>List (Unordered and Ordered)</li>
+        <li>Link dan Gambar</li>
+        <li>Tabel</li>
+    </ul>
+    <h2>Langkah Praktikum</h2>
+    <ol>
+        <li>Buat file index php</li>
+        <li>Tulis kode HTML</li>
+        <li>Simpan dan buka di browser</li>
+        <ul>
+            <li>buka xampp</li>
+            <li>nyalakan server</li>
+            <li>buka browser dengan url localhost/</li>
+        </ul>
+        <Li>Verifikasi hasil</Li>
+    </ol>
+
+</body>
+</html>
